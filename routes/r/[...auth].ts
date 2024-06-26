6f3ff30536ffa8cdd0710230a86d5a3b4cd74d35
De Gitgit Agitya <degitgitagitya@gmail.com>
2024-06-27 06:19:58 +0700

    feat(provider): add auth0

--- routes/r/[...auth].ts
+++ routes/r/[...auth].ts
@@ -1,6 +1,7 @@
 import { Auth } from '@auth/core'
 import Discord from '@auth/core/providers/discord'
 import Google from '@auth/core/providers/google'
+import Auth0 from '@auth/core/providers/auth0'
 import { eventHandler, toWebRequest } from 'h3'
 
 export default eventHandler(async (event) =>
@@ -10,6 +11,7 @@ export default eventHandler(async (event) =>
     trustHost: !!process.env.VERCEL,
     redirectProxyUrl: process.env.AUTH_REDIRECT_PROXY_URL,
     providers: [
+      Auth0,
       Discord({
         clientId: process.env.AUTH_DISCORD_ID,
         clientSecret: process.env.AUTH_DISCORD_SECRET
